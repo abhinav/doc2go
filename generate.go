abhinav/doc2go

commit 41d0ad96a22042a43169b3ea2c1aab22d91e0812

Add layout around generated HTML
Add a basic layout around generated HTML
and an -embed flag to opt-out of the layout.

Static content gets generated into a '_' directory,
and included with relative imports.

There is no custom styling at this time.

generate.go
@@ -41,8 +41,9 @@ var _ Assembler = (*godoc.Assembler)(nil)
 
 // Renderer renders a Go package's documentation to HTML.
 type Renderer interface {
+	WriteStatic(string) error
 	RenderPackage(io.Writer, *html.PackageInfo) error
-	RenderSubpackages(io.Writer, []*html.Subpackage) error
+	RenderPackageIndex(io.Writer, *html.PackageIndex) error
 }
 
 var _ Renderer = (*html.Renderer)(nil)
@@ -60,11 +61,17 @@ type Generator struct {
 	Renderer  Renderer
 	OutDir    string
 	Internal  bool
+	Embedded  bool
 	DocLinker godoc.Linker
 }
 
 // Generate runs the generator over the provided packages.
 func (r *Generator) Generate(pkgRefs []*gosrc.PackageRef) error {
+	if !r.Embedded {
+		if err := r.Renderer.WriteStatic(filepath.Join(r.OutDir, "_")); err != nil {
+			return err
+		}
+	}
 	return r.renderTrees(buildTrees(pkgRefs))
 }
 
@@ -141,7 +148,10 @@ func (r *Generator) writePackageIndex(w io.Writer, from string, rpkgs []*rendere
 		return nil
 	}
 
-	return r.Renderer.RenderSubpackages(w, subpkgs)
+	return r.Renderer.RenderPackageIndex(w, &html.PackageIndex{
+		Path:     from,
+		Packages: subpkgs,
+	})
 }
 
 type renderedPackage struct {

internal/html/render.go
@@ -8,31 +8,51 @@ import (
 	"go/doc/comment"
 	"html/template"
 	"io"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
 
 	"go.abhg.dev/doc2go/internal/godoc"
+	"go.abhg.dev/doc2go/internal/relative"
 )
 
 var (
 	//go:embed tmpl/*.html
 	_tmplFS embed.FS
 
-	_tmpl = template.Must(
-		template.New("").
-			Funcs(template.FuncMap{
-				// Trick borrowed from pkgsite:
-				// Unusable function references at parse time,
-				// and then Clone and replace at render time.
-				// This way, template validity is still
-				// verified at init.
-				"doc":  (*packageRenderer).doc,
-				"code": renderCode,
-			}).
-			ParseFS(_tmplFS, "tmpl/*"),
+	//go:embed static/**
+	_staticFS embed.FS
+
+	// Trick borrowed from pkgsite:
+	// Unusable function references at parse time,
+	// and then Clone and replace at render time.
+	// This way, template validity is still
+	// verified at init.
+	_packageTmpl = template.Must(
+		template.New("package.html").
+			Funcs((*render)(nil).FuncMap()).
+			ParseFS(_tmplFS, "tmpl/package.html", "tmpl/layout.html"),
+	)
+
+	_packageIndexTmpl = template.Must(
+		template.New("subpackages.html").
+			Funcs((*render)(nil).FuncMap()).
+			ParseFS(_tmplFS, "tmpl/subpackages.html", "tmpl/layout.html"),
 	)
 )
 
 // Renderer renders components into HTML.
-type Renderer struct{}
+type Renderer struct {
+	Embedded bool
+}
+
+func (r *Renderer) templateName() string {
+	if r.Embedded {
+		return "Body"
+	}
+	return "Page"
+}
 
 // DocPrinter formats godoc comments as HTML.
 type DocPrinter interface {
@@ -40,6 +60,31 @@ type DocPrinter interface {
 }
 
 var _ DocPrinter = (*comment.Printer)(nil)
+
+// WriteStatic dumps the contents of static/ into the given directory.
+func (*Renderer) WriteStatic(dir string) error {
+	static, err := fs.Sub(_staticFS, "static")
+	if err != nil {
+		return err
+	}
+	return fs.WalkDir(static, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil || path == "." {
+			return err
+		}
+
+		outPath := filepath.Join(dir, path)
+		if d.IsDir() {
+			return os.MkdirAll(outPath, 0o1755)
+		}
+
+		bs, err := fs.ReadFile(static, path)
+		if err != nil {
+			return err
+		}
+
+		return os.WriteFile(outPath, bs, 0o644)
+	})
+}
 
 // PackageInfo specifies the package that should be rendered.
 type PackageInfo struct {
@@ -52,14 +97,21 @@ type PackageInfo struct {
 
 // RenderPackage renders the documentation for a single Go package.
 // It does not include subpackage information.
-func (*Renderer) RenderPackage(w io.Writer, info *PackageInfo) error {
-	pkgRender := packageRenderer{
+func (r *Renderer) RenderPackage(w io.Writer, info *PackageInfo) error {
+	render := render{
+		Path:       info.Package.ImportPath,
 		DocPrinter: info.DocPrinter,
 	}
+	return template.Must(_packageTmpl.Clone()).
+		Funcs(render.FuncMap()).
+		ExecuteTemplate(w, r.templateName(), info.Package)
+}
 
-	return template.Must(_tmpl.Clone()).Funcs(template.FuncMap{
-		"doc": pkgRender.doc,
-	}).ExecuteTemplate(w, "package.html", info.Package)
+// PackageIndex holds information about a package listing.
+type PackageIndex struct {
+	// Path to this package index.
+	Path     string
+	Packages []*Subpackage
 }
 
 // Subpackage is a descendant of a Go package.
@@ -79,24 +131,41 @@ type Subpackage struct {
 	Synopsis string
 }
 
-// RenderSubpackages renders the list of descendants for a package
+// RenderPackageIndex renders the list of descendants for a package
 // as HTML.
-func (*Renderer) RenderSubpackages(w io.Writer, pkgs []*Subpackage) error {
-	return template.Must(_tmpl.Clone()).ExecuteTemplate(w, "subpackages.html", struct {
-		Subpackages []*Subpackage
-	}{Subpackages: pkgs})
+func (r *Renderer) RenderPackageIndex(w io.Writer, pidx *PackageIndex) error {
+	render := render{
+		Path: pidx.Path,
+	}
+	return template.Must(_packageIndexTmpl.Clone()).
+		Funcs(render.FuncMap()).
+		ExecuteTemplate(w, r.templateName(), pidx)
 }
 
-type packageRenderer struct {
+type render struct {
+	Path string
+
 	// DocPrinter converts Go comment.Doc objects into HTML.
 	DocPrinter DocPrinter
 }
 
-func (r *packageRenderer) doc(doc *comment.Doc) template.HTML {
+func (r *render) FuncMap() template.FuncMap {
+	return template.FuncMap{
+		"doc":    r.doc,
+		"code":   r.code,
+		"static": r.static,
+	}
+}
+
+func (r *render) static(p string) string {
+	return relative.Path(r.Path, path.Join("_", p))
+}
+
+func (r *render) doc(doc *comment.Doc) template.HTML {
 	return template.HTML(r.DocPrinter.HTML(doc))
 }
 
-func renderCode(code *godoc.Code) template.HTML {
+func (*render) code(code *godoc.Code) template.HTML {
 	var buf bytes.Buffer
 	for _, b := range code.Spans {
 		switch b := b.(type) {

internal/html/tmpl/layout.html
@@ -1,0 +1,14 @@
+{{ define "Page" -}}
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ static "css/main.css" }}" rel="stylesheet" />
+    {{ template "Head" $ -}}
+  </head>
+  <body>
+    <main>{{ template "Body" $ -}}</main>
+  </body>
+</html>
+{{ end -}}

internal/html/tmpl/package.html
@@ -1,3 +1,9 @@
+{{ define "Head" -}}
+<title>{{ .Name }}</title>
+{{- /* TODO: For binaries, use the binary name. */ -}}
+{{ end -}}
+
+{{ define "Body" -}}
 <h2 id="pkg-overview">package {{ .Name }}</h2>
 <p><code>import "{{ .ImportPath }}"</code></p>
 {{ .Doc | doc }}
@@ -75,6 +81,7 @@
       {{ template "funcOrMethod" . -}}
     {{ end -}}
   {{ end -}}
+{{ end -}}
 {{ end -}}
 
 {{- define "constOrVar" -}}

internal/html/tmpl/subpackages.html
@@ -1,8 +1,13 @@
+{{ define "Head" -}}
+<title>{{ .Path }}</title>
+{{ end -}}
+
+{{ define "Body" -}}
 <h2 id="pkg-directories">Directories</h2>
 
 <table>
   <tbody>
-    {{ range .Subpackages -}}
+    {{ range .Packages -}}
       <tr>
         <td><a href="{{ .RelativePath }}">{{ .RelativePath }}</a></td>
         <td>
@@ -12,3 +17,4 @@
     {{ end -}}
   </tbody>
 </table>
+{{ end -}}

flags.go
@@ -20,6 +20,7 @@ type params struct {
 	OutputDir string
 	Patterns  []string
 	Internal  bool
+	Embedded  bool
 
 	PackageDocTemplates []pathTemplate
 }
@@ -59,6 +60,9 @@ func (cmd *cliParser) newFlagSet() (*params, *flag.FlagSet) {
 		"We always generate documentation for internal packages,\n"+
 		"but by default, we do not include them in package lists.\n"+
 		"Use this flag to have them listed.")
+	flag.BoolVar(&p.Embedded, "embed", false, "generate partial HTML pages fit for embedding.\n"+
+		"Instead of generating a standalone HTML website, generate partial HTML pages\n"+
+		"that can be incorporated into a website using a static site generator.")
 	flag.Var(flagvalue.ListOf(&p.PackageDocTemplates), "pkg-doc", "use TEMPLATE to generate documentation links for PATH and its children.\n"+
 		"  -pkg-doc example.com=https://godoc.example.com/{{.ImportPath}}\n"+
 		"The argument must be in the form `PATH=TEMPLATE`.\n"+

internal/html/static/css/main.css
@@ -1,0 +1,1 @@
+/* TODO */

main.go
@@ -99,9 +99,12 @@ func (cmd *mainCmd) run(opts *params) error {
 		Assembler: &godoc.Assembler{
 			Linker: &linker,
 		},
-		Renderer:  new(html.Renderer),
+		Renderer: &html.Renderer{
+			Embedded: opts.Embedded,
+		},
 		OutDir:    opts.OutputDir,
 		Internal:  opts.Internal,
+		Embedded:  opts.Embedded,
 		DocLinker: &linker,
 	}
 

commit cdd2b84bb9c65be216ac1c263718353992590f25

page: Generate breadcrumbs
Include breadcrumbs for parent pages
in the generated standalone HTML pages.

This also addresses a bug introduced in 41d0ad9
that would cause a full page to be rendered
for subdirectory listings inside full packages.

generate.go
@@ -73,14 +73,14 @@ func (r *Generator) Generate(pkgRefs []*gosrc.PackageRef) error {
 		}
 	}
 
-	_, err := r.renderTrees(buildTrees(pkgRefs))
+	_, err := r.renderTrees(nil, buildTrees(pkgRefs))
 	return err
 }
 
-func (r *Generator) renderTrees(trees []packageTree) ([]*renderedPackage, error) {
+func (r *Generator) renderTrees(crumbs []html.Breadcrumb, trees []packageTree) ([]*renderedPackage, error) {
 	var pkgs []*renderedPackage
 	for _, t := range trees {
-		rpkgs, err := r.renderTree(t)
+		rpkgs, err := r.renderTree(crumbs, t)
 		if err != nil {
 			return nil, err
 		}
@@ -89,19 +89,27 @@ func (r *Generator) renderTrees(trees []packageTree) ([]*renderedPackage, error)
 	return pkgs, nil
 }
 
-func (r *Generator) renderTree(t packageTree) ([]*renderedPackage, error) {
-	if t.Value == nil {
-		return r.renderPackageIndex(t)
+func (r *Generator) renderTree(crumbs []html.Breadcrumb, t packageTree) ([]*renderedPackage, error) {
+	var crumbText string
+	if n := len(crumbs); n > 0 {
+		crumbText = relative.Path(crumbs[n-1].Path, t.Path)
+	} else {
+		crumbText = t.Path
 	}
-	rpkg, err := r.renderPackage(t)
+	crumbs = append(crumbs, html.Breadcrumb{Text: crumbText, Path: t.Path})
+
+	if t.Value == nil {
+		return r.renderPackageIndex(crumbs, t)
+	}
+	rpkg, err := r.renderPackage(crumbs, t)
 	if err != nil {
 		return nil, err
 	}
 	return []*renderedPackage{rpkg}, nil
 }
 
-func (r *Generator) renderPackageIndex(t packageTree) ([]*renderedPackage, error) {
-	subpkgs, err := r.renderTrees(t.Children)
+func (r *Generator) renderPackageIndex(crumbs []html.Breadcrumb, t packageTree) ([]*renderedPackage, error) {
+	subpkgs, err := r.renderTrees(crumbs, t.Children)
 	if err != nil {
 		return nil, err
 	}
@@ -119,39 +127,16 @@ func (r *Generator) renderPackageIndex(t packageTree) ([]*renderedPackage, error
 	}
 	defer f.Close()
 
-	if err := r.writePackageIndex(f, t.Path, subpkgs); err != nil {
+	idx := html.PackageIndex{
+		Path:        t.Path,
+		Subpackages: r.subpackages(t.Path, subpkgs),
+		Breadcrumbs: crumbs,
+	}
+	if err := r.Renderer.RenderPackageIndex(f, &idx); err != nil {
 		return nil, err
 	}
 
 	return subpkgs, nil
-}
-
-func (r *Generator) writePackageIndex(w io.Writer, from string, rpkgs []*renderedPackage) error {
-	var subpkgs []*html.Subpackage
-	for _, rpkg := range rpkgs {
-		// TODO: track this on packageTree?
-		relPath := relative.Filepath(from, rpkg.ImportPath)
-
-		if relPath == "internal" || strings.HasPrefix(relPath, "internal/") {
-			if !r.Internal {
-				continue
-			}
-		}
-
-		subpkgs = append(subpkgs, &html.Subpackage{
-			RelativePath: relPath,
-			Synopsis:     rpkg.Synopsis,
-		})
-	}
-
-	if len(subpkgs) == 0 {
-		return nil
-	}
-
-	return r.Renderer.RenderPackageIndex(w, &html.PackageIndex{
-		Path:     from,
-		Packages: subpkgs,
-	})
 }
 
 type renderedPackage struct {
@@ -159,8 +144,8 @@ type renderedPackage struct {
 	Synopsis   string
 }
 
-func (r *Generator) renderPackage(t packageTree) (*renderedPackage, error) {
-	subpkgs, err := r.renderTrees(t.Children)
+func (r *Generator) renderPackage(crumbs []html.Breadcrumb, t packageTree) (*renderedPackage, error) {
+	subpkgs, err := r.renderTrees(crumbs, t.Children)
 	if err != nil {
 		return nil, err
 	}
@@ -190,7 +175,9 @@ func (r *Generator) renderPackage(t packageTree) (*renderedPackage, error) {
 	defer f.Close()
 
 	info := html.PackageInfo{
-		Package: dpkg,
+		Package:     dpkg,
+		Breadcrumbs: crumbs,
+		Subpackages: r.subpackages(dpkg.ImportPath, subpkgs),
 		DocPrinter: &comment.Printer{
 			DocLinkURL: func(link *comment.DocLink) string {
 				return r.DocLinker.DocLinkURL(dpkg.ImportPath, link)
@@ -201,14 +188,30 @@ func (r *Generator) renderPackage(t packageTree) (*renderedPackage, error) {
 		return nil, fmt.Errorf("render: %w", err)
 	}
 
-	if err := r.writePackageIndex(f, t.Path, subpkgs); err != nil {
-		return nil, err
-	}
-
 	return &renderedPackage{
 		ImportPath: ref.ImportPath,
 		Synopsis:   dpkg.Synopsis,
 	}, nil
+}
+
+func (r *Generator) subpackages(from string, rpkgs []*renderedPackage) []html.Subpackage {
+	subpkgs := make([]html.Subpackage, 0, len(rpkgs))
+	for _, rpkg := range rpkgs {
+		// TODO: track this on packageTree?
+		relPath := relative.Filepath(from, rpkg.ImportPath)
+
+		if relPath == "internal" || strings.HasPrefix(relPath, "internal/") {
+			if !r.Internal {
+				continue
+			}
+		}
+
+		subpkgs = append(subpkgs, html.Subpackage{
+			RelativePath: relPath,
+			Synopsis:     rpkg.Synopsis,
+		})
+	}
+	return subpkgs
 }
 
 type packageTree = pathtree.Snapshot[*gosrc.PackageRef]

internal/html/render.go
@@ -32,13 +32,14 @@ var (
 	_packageTmpl = template.Must(
 		template.New("package.html").
 			Funcs((*render)(nil).FuncMap()).
-			ParseFS(_tmplFS, "tmpl/package.html", "tmpl/layout.html"),
+			ParseFS(_tmplFS,
+				"tmpl/package.html", "tmpl/layout.html", "tmpl/subpackages.html"),
 	)
 
 	_packageIndexTmpl = template.Must(
-		template.New("subpackages.html").
+		template.New("directory.html").
 			Funcs((*render)(nil).FuncMap()).
-			ParseFS(_tmplFS, "tmpl/subpackages.html", "tmpl/layout.html"),
+			ParseFS(_tmplFS, "tmpl/directory.html", "tmpl/layout.html", "tmpl/subpackages.html"),
 	)
 )
 
@@ -86,10 +87,23 @@ func (*Renderer) WriteStatic(dir string) error {
 	})
 }
 
+// Breadcrumb holds information about parents of a page
+// so that we can leave a trail up for navigation.
+type Breadcrumb struct {
+	// Text for the crumb.
+	Text string
+
+	// Path to the crumb from the root of the output.
+	Path string
+}
+
 // PackageInfo specifies the package that should be rendered.
 type PackageInfo struct {
 	// Parsed package documentation information.
-	Package *godoc.Package
+	*godoc.Package
+
+	Subpackages []Subpackage
+	Breadcrumbs []Breadcrumb
 
 	// DocPrinter specifies how to render godoc comments.
 	DocPrinter DocPrinter
@@ -99,19 +113,21 @@ type PackageInfo struct {
 // It does not include subpackage information.
 func (r *Renderer) RenderPackage(w io.Writer, info *PackageInfo) error {
 	render := render{
-		Path:       info.Package.ImportPath,
+		Path:       info.ImportPath,
 		DocPrinter: info.DocPrinter,
 	}
 	return template.Must(_packageTmpl.Clone()).
 		Funcs(render.FuncMap()).
-		ExecuteTemplate(w, r.templateName(), info.Package)
+		ExecuteTemplate(w, r.templateName(), info)
 }
 
 // PackageIndex holds information about a package listing.
 type PackageIndex struct {
 	// Path to this package index.
-	Path     string
-	Packages []*Subpackage
+	Path string
+
+	Subpackages []Subpackage
+	Breadcrumbs []Breadcrumb
 }
 
 // Subpackage is a descendant of a Go package.
@@ -151,14 +167,19 @@ type render struct {
 
 func (r *render) FuncMap() template.FuncMap {
 	return template.FuncMap{
-		"doc":    r.doc,
-		"code":   r.code,
-		"static": r.static,
+		"doc":          r.doc,
+		"code":         r.code,
+		"static":       r.static,
+		"relativePath": r.relativePath,
 	}
 }
 
+func (r *render) relativePath(p string) string {
+	return relative.Path(r.Path, p)
+}
+
 func (r *render) static(p string) string {
-	return relative.Path(r.Path, path.Join("_", p))
+	return r.relativePath(path.Join("_", p))
 }
 
 func (r *render) doc(doc *comment.Doc) template.HTML {

internal/html/tmpl/directory.html
@@ -1,0 +1,7 @@
+{{ define "Head" -}}
+  <title>{{ .Path }}</title>
+{{ end -}}
+
+{{ define "Body" -}}
+  {{ template "subpackages.html" . -}}
+{{ end -}}

internal/html/tmpl/layout.html
@@ -8,6 +8,14 @@
     {{ template "Head" $ -}}
   </head>
   <body>
+    {{ range $idx, $crumb := .Breadcrumbs -}}
+      {{ if gt $idx 0 }}/{{ end -}}
+      {{ with (relativePath .Path) -}}
+        <a href="{{ . }}">{{ $crumb.Text }}</a>
+      {{- else -}}
+        {{ $crumb.Text -}}
+      {{ end -}}
+    {{- end }}
     <main>{{ template "Body" $ -}}</main>
   </body>
 </html>

internal/html/tmpl/package.html
@@ -82,6 +82,10 @@
     {{ end -}}
   {{ end -}}
 {{ end -}}
+
+{{ if .Subpackages -}}
+  {{ template "subpackages.html" . -}}
+{{ end -}}
 {{ end -}}
 
 {{- define "constOrVar" -}}

internal/html/tmpl/subpackages.html
@@ -1,13 +1,8 @@
-{{ define "Head" -}}
-<title>{{ .Path }}</title>
-{{ end -}}
-
-{{ define "Body" -}}
-<h2 id="pkg-directories">Directories</h2>
+<h3 id="pkg-directories">Directories</h3>
 
 <table>
   <tbody>
-    {{ range .Packages -}}
+    {{ range .Subpackages -}}
       <tr>
         <td><a href="{{ .RelativePath }}">{{ .RelativePath }}</a></td>
         <td>
@@ -17,4 +12,3 @@
     {{ end -}}
   </tbody>
 </table>
-{{ end -}}