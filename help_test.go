890f1cbfb76b9e0704e60ac7d1111e978cbd2b76
Abhinav Gupta <mail@abhinavg.net>
2023-01-08 15:05:19 -0800

    main/help: Tests (#44)
    Missed help_test.go in #42

---
+++ help_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHelp_Write(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		give    Help
+		wantErr string
+	}{
+		{give: UsageHelp},
+		{give: DefaultHelp},
+		{give: FrontMatterHelp},
+		{give: PackageDocHelp},
+		{
+			give:    "not-a-topic",
+			wantErr: `unknown help topic "not-a-topic": valid values`,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.give.String(), func(t *testing.T) {
+			t.Parallel()
+
+			err := tt.give.Write(io.Discard)
+			if len(tt.wantErr) > 0 {
+				assert.ErrorContains(t, err, tt.wantErr)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
