c6cb85e7faada2d9cff03dda281a68982cfa6c3a
Abhinav Gupta <mail@abhinavg.net>
2024-02-24 10:03:08 -0800

    docs: Add a godoc-tricks example (#213)
    Adds an example demonstrating the various features of godoc
    with the godoc-tricks repository.
    
    Thanks to @oliverpool for this sugggestion.
    
    Resolves #197

---
+++ docs/doc.go
@@ -0,0 +1,8 @@
+// Package docs generates documentation for doc2go.
+//
+// The Go package exists just to isolate the documentation
+// from the actual code,
+// and to add any dependencies needed for documentation generation.
+package docs
+
+import _ "github.com/fluhus/godoc-tricks"
