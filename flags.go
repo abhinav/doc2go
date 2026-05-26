f22b548de0aa8955cb22076f86dd5a7fb829fd28
Quin Gillespie <trypsynth@gmail.com>
2026-05-26 09:18:27 -0600

    Add -o as a shorthand for --out (#354)

--- flags.go
+++ flags.go
@@ -81,6 +81,7 @@ func (cmd *cliParser) newFlagSet(cfg *configFileParser) (*params, *flag.FlagSet)
 
 	// Filesystem:
 	flag.StringVar(&p.OutputDir, "out", "_site", "")
+	flag.Var(flag.Lookup("out").Value, "o", "")
 	flag.StringVar(&p.SubDir, "subdir", "", "")
 	flag.StringVar(&p.PkgVersion, "pkg-version", "", "")
 	flag.StringVar(&p.Basename, "basename", "", "")
